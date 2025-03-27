a24ca187951b3dd91b8a219f5f58ec80e3c5592f
Михаил_Тарбеев <tarb3@yandex.ru>
2025-03-27 23:12:45 +0300

    84 страница книги

--- first_app_asyncio/main.py
+++ first_app_asyncio/main.py
@@ -9,8 +9,38 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Задание адреса сокета
 address = ('127.0.0.1', 8000)
 server_socket.bind(address)
-
 # Прослушивание запросов на подключение
 server_socket.listen()
-connection, client_address = server_socket.accept()
-print(f'Получен запрос на подключение от {client_address}')
+
+# Задание сокета неблокирующим
+server_socket.setblocking(False)
+
+connections = []
+
+try:
+    while True:
+        try:
+            connection, client_address = server_socket.accept()
+            connection.setblocking(False)
+            print(f'I got a connection from {client_address}!')
+            connections.append(connection)
+        except BlockingIOError:
+            pass
+        for connection in connections:
+            buffer = b''
+
+            while buffer[-2:] != b'\r\n':
+                try:
+                    data = connection.recv(2)
+                    if not data:
+                        break
+                    else:
+                        print(f'I got data: {data}!')
+                        buffer = buffer + data
+                    print(f"All the data is: {buffer}")
+                except BlockingIOError:
+                    pass
+            connection.send(buffer)
+
+finally:
+    server_socket.close()
